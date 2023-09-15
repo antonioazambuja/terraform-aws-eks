67905cdf09ed74d9f14e3a359bb30d678b4b695e
Antonio Azambuja <antonioazambuja_@hotmail.com>
2023-09-15 18:26:26 -0300

    add charts url and version variables

--- variables.tf
+++ variables.tf
@@ -136,3 +136,23 @@ variable "tags" {
     error_message = "Tags from EKS Cluster is empty."
   }
 }
+
+variable "istio_chart_version" {
+  default = "1.18.1"
+  type    = string
+}
+
+variable "istio_chart_url" {
+  default = "https://istio-release.storage.googleapis.com/charts"
+  type    = string
+}
+
+variable "kiali_chart_url" {
+  default = "https://kiali.org/helm-charts"
+  type    = string
+}
+
+variable "prometheus_chart_url" {
+  default = "https://prometheus-community.github.io/helm-charts"
+  type    = string
+}
