492a6d867e19edf86c3d8f464b52c5a569aedbc3
Antonio Azambuja <antonioazambuja_@hotmail.com>
2023-09-15 18:33:25 -0300

    fix dependency on node group and eks cluster in the creating and deletion

--- node_group.tf
+++ node_group.tf
@@ -16,7 +16,8 @@ resource "aws_eks_node_group" "eks_node_group" {
     depends_on = [
         aws_iam_role_policy_attachment.eks_AmazonEKSWorkerNodePolicy,
         aws_iam_role_policy_attachment.eks_AmazonEKS_CNI_Policy,
-        aws_iam_role_policy_attachment.eks_AmazonEC2ContainerRegistryReadOnly
+        aws_iam_role_policy_attachment.eks_AmazonEC2ContainerRegistryReadOnly,
+        aws_eks_cluster.eks_cluster
     ]
 
 }
